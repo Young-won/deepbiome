eaa34110b7e865dbb1eaae3473e4e819d4c354ba
Young-won <youngwon08@gmail.com>
2019-10-23 16:09:33 +0900

    organize the examples

--- examples/simulation_s5/simulation_s5_deepbiome_noise_decay/run.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=2
-echo $CUDA_VISIBLE_DEVICES
-
-model=${PWD##*/}
-echo $model
-
-python3 ../../main.py --kfold=5 --log_info=config/log_info.yaml --path_info=config/path_info.cfg --network_info=config/network_info.cfg  --max_queue_size=50 --workers=10 --use_multiprocessing=False
