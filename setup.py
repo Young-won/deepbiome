e16c6d0419bd10e6719f6c93f0c26c378a157662
Young-won <youngwon08@gmail.com>
2019-10-23 22:55:30 +0900

    add test data

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
         'deepbiome': [
             # When adding files here, remember to update MANIFEST.in as well,
             # or else they will not be included in the distribution on PyPI!
-            # 'path/to/data_file',
+            'deepbiome/tests/data/*',
         ]
     },
     install_requires=requirements,
