6cdd971a41ad986fdecfb8d9a9de33f3a1e9de5d
Young-won <youngwon08@gmail.com>
2019-10-24 21:39:38 +0900

    undo flake8

--- examples/main.py
+++ examples/main.py
@@ -1,13 +1,14 @@
-######################################################################
-## DeepBiome
-## - Main code
-##
-## July 10. 2019
-## Youngwon (youngwon08@gmail.com)
-##
-## Reference
-## - Keras (https://github.com/keras-team/keras)
-######################################################################
+"""
+DeepBiome
+- Main code
+
+July 10. 2019
+Youngwon (youngwon08@gmail.com)
+
+Reference
+- Keras (https://github.com/keras-team/keras)
+"""
+
 import sys
 
 from deepbiome import configuration
@@ -15,7 +16,7 @@ from deepbiome import logging_daily
 from deepbiome import deepbiome
 from deepbiome.utils import argv_parse
 
-######################################################################
+# Argument ##########################################################
 argdict = argv_parse(sys.argv)
 try: gpu_memory_fraction = float(argdict['gpu_memory_fraction'][0]) 
 except: gpu_memory_fraction = None
@@ -26,7 +27,7 @@ except: workers=1
 try: use_multiprocessing=argdict['use_multiprocessing'][0]=='True'      
 except: use_multiprocessing=False
 
-### Logger ###########################################################
+# Logger ###########################################################
 logger = logging_daily.logging_daily(argdict['log_info'][0])
 logger.reset_logging()
 log = logger.get_logging()
@@ -36,7 +37,7 @@ log.info('Argument input')
 for argname, arg in argdict.items():
     log.info('    {}:{}'.format(argname,arg))
 
-### Configuration ####################################################
+# Configuration ####################################################
 config_data = configuration.Configurator(argdict['path_info'][0], log)
 config_data.set_config_map(config_data.get_section_map())
 config_data.print_config_map()
@@ -48,4 +49,3 @@ config_network.print_config_map()
 path_info = config_data.get_config_map()
 network_info = config_network.get_config_map()
 test_evaluation, train_evaluation, network = deepbiome.deepbiome_train(log, network_info, path_info, number_of_fold=10)
-######################################################################
