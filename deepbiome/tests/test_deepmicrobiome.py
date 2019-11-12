e2678ec012ff53c7e5cde64675d42829959701c3
Young-won <youngwon08@gmail.com>
2019-11-12 09:26:09 +0900

    add test functions for dnn and dnn_l1

--- deepbiome/tests/test_deepmicrobiome.py
+++ deepbiome/tests/test_deepmicrobiome.py
@@ -196,3 +196,47 @@ def test_deepbiome_regression_training(input_value_regression_training, output_v
     log.info(train_evaluation)
     log.info(np.all(np.isclose(real_train_evaluation, train_evaluation)))
     # assert np.all(np.isclose(real_test_evaluation, test_evaluation)) & np.all(np.isclose(real_train_evaluation, train_evaluation))
+    
+def test_deepbiome_regression_deep_training(input_value_regression_deep_training, output_value_regression):
+    '''
+    Test deepbiome by regression problem with simulated data
+    '''
+    log, network_info, path_info = input_value_regression_deep_training
+    
+    seed_value = 123
+    os.environ['PYTHONHASHSEED']=str(seed_value)
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    if tf.__version__.startswith('2'): tf.random.set_seed(seed_value)
+    else: tf.set_random_seed(seed_value)
+    test_evaluation, train_evaluation, network = deepbiome.deepbiome_train(log, network_info, path_info, number_of_fold=2)
+    # np.save('data/regression_real_train_evaluation.npy', train_evaluation)
+    # np.save('data/regression_real_test_evaluation.npy', test_evaluation)
+    
+    log.info('test')
+    log.info(test_evaluation)
+    
+    log.info('training')
+    log.info(train_evaluation)
+    
+def test_deepbiome_regression_deep_l1_training(input_value_regression_deep_l1_training, output_value_regression):
+    '''
+    Test deepbiome by regression problem with simulated data
+    '''
+    log, network_info, path_info = input_value_regression_deep_l1_training
+    
+    seed_value = 123
+    os.environ['PYTHONHASHSEED']=str(seed_value)
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    if tf.__version__.startswith('2'): tf.random.set_seed(seed_value)
+    else: tf.set_random_seed(seed_value)
+    test_evaluation, train_evaluation, network = deepbiome.deepbiome_train(log, network_info, path_info, number_of_fold=2)
+    # np.save('data/regression_real_train_evaluation.npy', train_evaluation)
+    # np.save('data/regression_real_test_evaluation.npy', test_evaluation)
+    
+    log.info('test')
+    log.info(test_evaluation)
+    
+    log.info('training')
+    log.info(train_evaluation)
