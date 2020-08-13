ac24df7d770c8ac13ac30ee64545cf1c784c75ca
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-08-13 16:37:32 +0000

    Update docs after building Travis build 137 of Young-won/deepbiome
    The docs were built from the branch 'master' against the commit
    3b3e40ebea07192782be37f9bf705e1781118779.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/Young-won/deepbiome/jobs/717654670.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.0/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0+8.gbe5563e',
+    VERSION: '1.0.0+9.g3b3e40e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
